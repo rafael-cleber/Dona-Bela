6541fef81864bcef92a5c491bc6c1719ad38096f
rafael-cleber <rafaelcleber@hotmail.com>
2024-01-09 10:39:37 -0300

    iniciando

---
+++ Projeto/index.php
@@ -0,0 +1,15 @@
+<?php  include"conecta.inc.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel Acapulco</title>
+</head>
+<body>
+    <?php include_once('topo.php')?>
+
+
+
+</body>
+</html>
